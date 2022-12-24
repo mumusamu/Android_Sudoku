9a610d6856a5dfbd83ad47d0f227e183c2efc592
mumusamu <mostafadarsh709@gmail.com>
2022-12-24 20:58:02 +0200

    MainActivity XML and some buttom theming

--- app/src/main/java/com/example/android_sudoku/SettingsActivity.kt
+++ app/src/main/java/com/example/android_sudoku/SettingsActivity.kt
@@ -3,10 +3,9 @@ package com.example.android_sudoku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_settings)
     }
 }
