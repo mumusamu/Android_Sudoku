f9de41eaa6511f033e0b502296591962f8474ae6
mumusamu <mostafadarsh709@gmail.com>
2022-12-29 15:10:49 +0200

    Added main activity code added settings activity code(incomplete) and xml removed button theme delay: resourced linking failed

--- app/src/main/java/com/example/android_sudoku/MainActivity.kt
+++ app/src/main/java/com/example/android_sudoku/MainActivity.kt
@@ -1,12 +1,75 @@
 package com.example.android_sudoku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.android_sudoku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // the binding variable is used to bind the xml file with the kt file
+    // is better than fin view by id
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view  = binding.root
+        setContentView(view)
+        //set up a shared preferences file if it doesnt exist already
+        var Shared_Prefs = getSharedPreferences(getString(R.string.Shred_Prefs_Name), MODE_PRIVATE)
+        if(!Shared_Prefs.getBoolean(getString(R.string.shared_prefs_validity),false)){
+            Toast.makeText(this
+                ,Shared_Prefs.getBoolean(getString(R.string.shared_prefs_validity),false).toString()
+                ,Toast.LENGTH_SHORT).show()
+            Set_Up_Shared_Prefs()
+        }
+        else{
+            Shared_Prefs.getString(getString(R.string.shared_prefs_theme_field),getString(R.string.Red))
+            Shared_Prefs.getInt(getString(R.string.shared_prefs_difficulty_field),1)
+            Shared_Prefs.getBoolean(getString(R.string.shared_prefs_volume_toggle), true)
+            Shared_Prefs.getBoolean(getString(R.string.shared_prefs_music_toggle), true)
+        }
+
+
+
+        // add a little mascot animation later (might cancel) # check sticky note ?
+        // get reference to x button
+        val NewGameBtn:Button = binding.NewGameBtn
+
+        // lambda expressin also this tells the button what happens when it's clicked
+        // might add a hover dialouge later ?? also needs mascot
+        NewGameBtn.setOnClickListener {
+            // intents are responsible for changing scenes and opening different activities
+            val i = Intent(this,GameActivity::class.java)
+            startActivity(i)
+
+        }
+        val ResumeGameBtn:Button = binding.ResumeGameBtn
+        ResumeGameBtn.setOnClickListener {
+            val i = Intent(this,GameActivity::class.java)
+            // addextra(data) ? for the save game data
+            startActivity(i)
+
+        }
+        val SettingsBtn:Button = binding.SettingsBtn
+        SettingsBtn.setOnClickListener {
+            val i = Intent(this,SettingsActivity::class.java)
+            startActivity(i)
+
+        }
+    }
+    fun Set_Up_Shared_Prefs()
+    {
+        var Shared_Prefs = getSharedPreferences(getString(R.string.Shred_Prefs_Name), MODE_PRIVATE)
+        var editor = Shared_Prefs.edit()
+        editor.putBoolean(getString(R.string.shared_prefs_validity),true)
+        editor.putString(getString(R.string.shared_prefs_theme_field),getString(R.string.Red))
+        editor.putString(getString(R.string.shared_prefs_difficulty_field),getString(R.string.Begienner))
+        editor.putBoolean(getString(R.string.shared_prefs_volume_toggle),true)
+        editor.putBoolean(getString(R.string.shared_prefs_music_toggle),true)
+        editor.commit()
 
     }
+
 }
--- app/src/main/java/com/example/android_sudoku/SettingsActivity.kt
+++ app/src/main/java/com/example/android_sudoku/SettingsActivity.kt
@@ -2,10 +2,66 @@ package com.example.android_sudoku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import com.example.android_sudoku.databinding.ActivityMainBinding
+import com.example.android_sudoku.databinding.ActivitySettingsBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class SettingsActivity : AppCompatActivity() {
+    lateinit var binding:ActivitySettingsBinding
+    lateinit var theme_dat:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        val view  = binding.root
+        setContentView(view)
+
+        val Themes = listOf(
+            getString(R.string.Red)
+            ,getString(R.string.Yellow)
+            ,getString(R.string.Green)
+            ,getString(R.string.Purple)
+            ,getString(R.string.Blue)
+            ,getString(R.string.Orange))
+
+        val Difficulty_Items = listOf(
+            getString(R.string.Begienner)
+            ,getString(R.string.Easy)
+            ,getString(R.string.Medium)
+            ,getString(R.string.Hard)
+            ,getString(R.string.Master)
+            ,getString(R.string.Legend))
+
+        Create_List_Item(Themes,binding.Theme,binding.ThemeData)
+        Create_List_Item(Difficulty_Items,binding.difficulty,binding.diffData)
+
     }
-}
+
+    fun Create_List_Item(String_List_Items:List<String> , Target_Item:TextInputLayout,ATV:AutoCompleteTextView){
+        val adapter = ArrayAdapter(this,R.layout.settings_dropdown_item,String_List_Items)
+        (Target_Item.editText as?  AutoCompleteTextView)?.setAdapter(adapter)
+        var Shared_Prefs = getSharedPreferences(getString(R.string.Shred_Prefs_Name), MODE_PRIVATE)
+        var editor = Shared_Prefs.edit()
+        if (ATV == binding.ThemeData){
+            ATV.setOnItemClickListener { parent, view, position, id ->
+                editor.putString(getString(R.string.shared_prefs_theme_field), ATV.text.toString())
+                editor.commit()
+                Toast.makeText(this,
+                    Shared_Prefs.getString(getString(R.string.shared_prefs_theme_field),"Wrong"),
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+        else if (ATV == binding.diffData){
+            ATV.setOnItemClickListener { parent, view, position, id ->
+                editor.putString(getString(R.string.shared_prefs_difficulty_field),ATV.text.toString())
+                editor.commit()
+                Toast.makeText(this,
+                    Shared_Prefs.getString(getString(R.string.shared_prefs_difficulty_field),"Wrong"),
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        }
+    }
